9202ce77cac709253199051d3e2c47434738b33d
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-01-28 22:04:57 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+
+var a = prompt('Wprowadź długość podstawy trójkąta');
+var	h = prompt('Wprowadź długość wysokości trójkąta');
+var triangleArea = a*h/2;
+alert('Pole trójkąta = ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
